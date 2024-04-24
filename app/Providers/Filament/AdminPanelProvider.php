54ba9548b14b3ceee1f951f196e333c533d7ce9a
Tobi F. <tf4482@gmail.com>
2024-04-24 23:08:54 +0200

    feat: :sparkles: Updated AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -23,13 +23,19 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
-            ->id('admin')
-            ->path('admin')
-            ->login()
-            ->colors([
-                'primary' => Color::Amber,
-            ])
+        ->default()
+        ->id('admin')
+        ->path('panel')
+        ->login()
+        ->registration()
+        ->passwordReset()
+        ->emailVerification()
+        ->profile(isSimple: false)
+        ->topNavigation()
+        ->brandName('Fully Integrated System Assistant (FISA)')
+        ->colors([
+            'primary' => Color::Violet,
+        ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -37,8 +43,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,

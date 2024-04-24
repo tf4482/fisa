dad5a059cbeb9e81ae0014433c1719b7547dfd2b
Tobi F. <tf4482@gmail.com>
2024-04-25 01:15:21 +0200

    refactor: :recycle: Refactored operating systems create page

--- app/Filament/Resources/OperatingSystemResource/Pages/CreateOperatingSystem.php
+++ app/Filament/Resources/OperatingSystemResource/Pages/CreateOperatingSystem.php
@@ -7,10 +7,10 @@ use Filament\Resources\Pages\CreateRecord;
 
 class CreateOperatingSystem extends CreateRecord
 {
+    protected static string $resource = OperatingSystemResource::class;
+
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
     }
-
-    protected static string $resource = OperatingSystemResource::class;
 }

3884c6e67230fca42189acd829ef84a27baff584
Tobi F. <tf4482@gmail.com>
2024-04-25 06:59:04 +0200

    feat: :sparkles: Created ping device command

---
+++ app/Console/Commands/PingNetworkConfigurationsCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Host;
+use App\Models\NetworkConfiguration;
+use Illuminate\Console\Command;
+
+class PingNetworkConfigurationsCommand extends Command
+{
+    protected $signature = 'network:ping';
+
+    protected $description = 'Ping all IP addresses in network configurations and update host status';
+
+    public function handle()
+    {
+        $networkConfigurations = NetworkConfiguration::all();
+
+        foreach ($networkConfigurations as $networkConfiguration) {
+            if ($networkConfiguration->pingcheck) {
+                $ipAddress = $networkConfiguration->ip_address;
+                $pingResult = $this->ping($ipAddress);
+
+                $host = Host::find($networkConfiguration->host_id);
+
+                if ($host) {
+                    $host->status = $pingResult;
+                    $host->save();
+                }
+            }
+        }
+    }
+
+    public function ping($ipAddress)
+    {
+        $isWindows = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
+
+        if ($isWindows) {
+            $response = shell_exec("ping -n 1 $ipAddress");
+            $reachable = strpos($response, 'TTL=') !== false;
+        } else {
+            $response = shell_exec("ping -c 1 $ipAddress");
+            $reachable = strpos($response, '1 received') !== false;
+        }
+
+        $this->info($reachable ? 'true' : 'false');
+
+        return $reachable;
+    }
+}

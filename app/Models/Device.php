09b73021da3a4d2fe16b2ce74f8c4b1d81f59197
Tobi F. <tf4482@gmail.com>
2024-04-26 23:06:11 +0200

    feat: :card_file_box: Added relation to device model

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Enums\DeviceType;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -13,13 +12,6 @@ class Device extends Model
 {
     use HasFactory;
     use HasUuids;
-
-    protected function casts(): array
-    {
-        return [
-            'device_type' => DeviceType::class,
-        ];
-    }
 
     protected $fillable = [
         'model',

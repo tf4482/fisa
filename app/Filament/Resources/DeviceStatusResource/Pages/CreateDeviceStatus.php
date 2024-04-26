4428cc8ce71b497fb9f483c00cb7a4d235a94173
Tobi F. <tf4482@gmail.com>
2024-04-26 22:58:18 +0200

    feat: :sparkles: Created resource

---
+++ app/Filament/Resources/DeviceStatusResource/Pages/CreateDeviceStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\DeviceStatusResource\Pages;
+
+use App\Filament\Resources\BaseCreateRecord;
+use App\Filament\Resources\DeviceStatusResource;
+
+class CreateDeviceStatus extends BaseCreateRecord
+{
+    protected static string $resource = DeviceStatusResource::class;
+}

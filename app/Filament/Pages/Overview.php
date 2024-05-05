f16cefab394a52d1ddba3025363f3a12edceb472
tf4482 <tf4482@gmail.com>
2024-05-05 18:22:25 +0200

    feat: :sparkles: Updated overview custom page

--- app/Filament/Pages/Overview.php
+++ app/Filament/Pages/Overview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Hosts;
 use Filament\Pages\Page;
 
 class Overview extends Page
@@ -9,6 +10,8 @@ class Overview extends Page
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.overview';
+
+    public $hosts;
 
     public function getTitle(): string
     {
@@ -18,5 +21,10 @@ class Overview extends Page
     public static function getNavigationLabel(): string
     {
         return __('Overview');
+    }
+
+    public function mount(): void
+    {
+        $this->hosts = Hosts::orderBy('name')->get();
     }
 }

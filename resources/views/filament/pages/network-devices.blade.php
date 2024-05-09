59bfd6d194fe5f724583b5cb4d97e1510c83513e
tf4482 <tf4482@gmail.com>
2024-05-09 09:53:12 +0200

    chore: :fire: Removed obsolete files

--- resources/views/filament/pages/network-devices.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament-panels::page>
-<div class="flex flex-wrap">
-        @foreach ($hosts as $host)
-            <div class="p-2">
-                <livewire:host-component :host="$host" />
-            </div>
-        @endforeach
-    </div>
-</x-filament-panels::page>
--- resources/views/livewire/host-component.blade.php
+++
@@ -1,5 +0,0 @@
-    <x-filament::card>
-        <div>Host: {{ $host->name }}</div>
-        <div>IP: {{ $host->ip }}</div>
-        <div>Status: {{ $host->status }}</div>
-    </x-filament::card>

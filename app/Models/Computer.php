5d171ff002f4be0ba3e43eeeab8b90a182945fbe
tf4482 <tf4482@gmail.com>
2024-05-10 09:40:54 +0200

    feat: :card_file_box: Updated computer model

--- app/Models/Computer.php
+++ app/Models/Computer.php
@@ -11,11 +11,18 @@ class Computer extends Model
     use HasFactory;
     use HasUuids;
 
+    protected $casts = [
+        'inspect' => 'boolean',
+    ];
+
     protected $fillable = [
         'name',
         'status',
         'ip',
         'mac',
         'type',
+        'avatar',
+        'inspect',
+        'last_check',
     ];
 }
